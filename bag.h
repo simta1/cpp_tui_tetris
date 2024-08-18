53914e8e38380aa4a414987f2b23a45b914091fe
simta1 <133639447+simta1@users.noreply.github.com>
2024-08-18 17:12:57 +0900

    feat: 7-bag 구현

--- bag.h
+++ bag.h
@@ -1,21 +1,35 @@
 #include <vector>
 #include <queue>
 #include <numeric>
+#include <algorithm>
 #include <cstdlib>
 #include <ctime>
 using namespace std;
 
-// TODO 7-bag 시스템 추가
 template <int N>
 class Bag {
 private:
+    vector<int> origList;
+    vector<int> inventory;
+
+    random_device rd;
+    mt19937 engine;
 
 public:
-    Bag() {
-        srand(time(0));
+    Bag() : origList(N), engine(rd()) {
+        iota(origList.begin(), origList.end(), 1);
+    }
+
+    void pack() {
+        shuffle(origList.begin(), origList.end(), engine);
+        inventory.insert(inventory.end(), origList.begin(), origList.end());
     }
 
     int takeOut() {
-        return rand() % N + 1;
+        if (inventory.empty()) pack();
+        
+        int tmp = inventory.back();
+        inventory.pop_back();
+        return tmp;
     }
 };
