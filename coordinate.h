33da30f91a6c4e28777343b4c0a3f0af77444349
simta1 <133639447+simta1@users.noreply.github.com>
2024-08-18 16:33:01 +0900

    fix: coordinate 수정

--- coordinate.h
+++ coordinate.h
@@ -9,23 +9,22 @@ struct Coordinate {
     Coordinate() = default;
     Coordinate(int x, int y) : x(x), y(y) {}
     
-    Coordinate move(int x, int y) {
+    void move(int x, int y) {
         this->x += x;
         this->y += y;
     }
     
     void rotate(Direction direction) {
+        int tmpX = x;
+        int tmpY = y;
+
         switch (direction) {
             case Direction::CLOCKWISE:
-                int tmpX = x;
-                int tmpY = y;
                 x = tmpY;
                 y = -tmpX;
                 break;
             
             case Direction::COUNTERCLOCKWISE:
-                int tmpX = x;
-                int tmpY = y;
                 x = -tmpY;
                 y = tmpX;
                 break;
