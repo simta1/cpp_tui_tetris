2867f9fe9fb025785a6590779a452546063404eb
simta1 <133639447+simta1@users.noreply.github.com>
2024-08-18 14:14:17 +0900

    fix: 회전방향 수정

--- coordinate.h
+++ coordinate.h
@@ -6,6 +6,7 @@ enum class Direction {
 struct Coordinate {
     int x, y;
     
+    Coordinate() = default;
     Coordinate(int x, int y) : x(x), y(y) {}
     
     Coordinate move(int x, int y) {
@@ -15,14 +16,14 @@ struct Coordinate {
     
     void rotate(Direction direction) {
         switch (direction) {
-            case Direction::COUNTERCLOCKWISE:
+            case Direction::CLOCKWISE:
                 int tmpX = x;
                 int tmpY = y;
                 x = tmpY;
                 y = -tmpX;
                 break;
             
-            case Direction::CLOCKWISE:
+            case Direction::COUNTERCLOCKWISE:
                 int tmpX = x;
                 int tmpY = y;
                 x = -tmpY;
