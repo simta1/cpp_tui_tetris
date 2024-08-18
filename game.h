218bb92696b73c500661dc4141bb7b1efb9382ef
simta1 <133639447+simta1@users.noreply.github.com>
2024-08-18 23:54:48 +0900

    feat: 화면 출력 최적화

--- game.h
+++ game.h
@@ -2,7 +2,8 @@
 #define GAME_H
 
 #include <bits/stdc++.h>
-#include "utils.h"
+#include "ConsoleColor.h"
+#include "lazyPrinter.h"
 #include "bag.h"
 #include "fallingBlock.h"
 #include "holdedBlock.h"
@@ -11,18 +12,26 @@
 #include "rotateDirection.h"
 using namespace std;
 
+// ms 기준
+const int dropTime = 500;
+const int hardDropAnimationTime = 40;
+const int breakRowAnimationTime = 130;
+
 // TODO nextblock 추가
 // TODO playTime 추가
-// TODO replay 추가
+// TODO gameover 시 replay 추가
 // TODO ghostblock 추가
+// TODO harddrop 시각효과 추가
 template <int ROWS, int COLS>
 class Game {
 private:
     // 출력 관련
-    const int LEN = 1; // 격자칸의 한 변에 사용될 픽셀 개수
-    const int MARGIN_HEIGHT = 3; // 보드판 상단에 들어갈 여백 길이(단위 : 격자개수)
-    const int MARGIN_WIDTH = 6; // 보드판 좌측에 들어갈 여백 길이(단위 : 격자개수)
+    static constexpr int LEN = 1; // 격자칸의 한 변에 사용될 픽셀 개수
+    static constexpr int MARGIN_HEIGHT = 3; // 보드판 상단에 들어갈 여백 길이(단위 : 격자개수)
+    static constexpr int MARGIN_WIDTH = 6; // 보드판 좌측에 들어갈 여백 길이(단위 : 격자개수)
     vector<pair<int, int> > borderPos;
+
+    LazyPrinter lazyPrinter;
 
     // 게임 로직 관련
     vector<vector<int> > board;
@@ -61,24 +70,25 @@ private:
     }
 
     void drawGrid(int x, int y) {
-        rect((MARGIN_WIDTH + x) * LEN, (MARGIN_HEIGHT + y) * LEN, LEN, LEN);
+        lazyPrinter.rect((MARGIN_WIDTH + x) * LEN, (MARGIN_HEIGHT + y) * LEN, LEN, LEN);
     }
 
     void deleteMargin() {
-        setColor(ConsoleColor::ORIGINALBG);
-        rect(MARGIN_WIDTH * LEN, 0, (COLS + 2) * LEN, MARGIN_HEIGHT * LEN);
-        rect(0, MARGIN_HEIGHT * LEN, MARGIN_WIDTH * LEN, (ROWS + 2) * LEN);
+        lazyPrinter.setColor(ConsoleColor::ORIGINALBG);
+        // lazyPrinter.rect(0, 0, (MARGIN_WIDTH + COLS + 2) * LEN, (MARGIN_HEIGHT + ROWS + 2) * LEN);
+        lazyPrinter.rect(MARGIN_WIDTH * LEN, 0, (COLS + 2) * LEN, MARGIN_HEIGHT * LEN);
+        lazyPrinter.rect(0, MARGIN_HEIGHT * LEN, MARGIN_WIDTH * LEN, (ROWS + 2) * LEN);
     }
 
     void drawBorder() {
-        setColor(ConsoleColor::BORDER_DEFAULT);
+        lazyPrinter.setColor(ConsoleColor::BORDER_DEFAULT);
         for (auto [i, j] : borderPos) drawGrid(i, j);
     }
 
     void drawBoard() {
         for (int i = 0; i < ROWS; i++) {
             for (int j = 0; j < COLS; j++) {
-                setColor(tetrominoColor[board[i][j]]);
+                lazyPrinter.setColor(tetrominoColor[board[i][j]]);
                 drawGrid(j + 1, i + 1);
             }
         }
@@ -86,31 +96,30 @@ private:
         // highlight Full Rows
         for (auto fullRow : fullRows) {
             for (int j = 0; j < COLS; j++) {
-                setColor(ConsoleColor::FULL_ROW_HIGHLIGHT, tetrominoColor[board[fullRow][j]]);
+                lazyPrinter.setColor(ConsoleColor::FULL_ROW_HIGHLIGHT, tetrominoColor[board[fullRow][j]]);
                 drawGrid(j + 1, fullRow + 1);
             }
         }
     }
     
     void drawFallingBlock() {
-        setColor(tetrominoColor[fallingBlock.getKind()]);
+        lazyPrinter.setColor(tetrominoColor[fallingBlock.getKind()]);
         for (auto [x, y] : fallingBlock.getCoordinates()) drawGrid(x + 1, y + 1);
     }
 
     void drawHold() {
         // hold Border
-        setColor(ConsoleColor::BORDER_DEFAULT);
-        gotoxy(MARGIN_WIDTH / 2 * LEN * PIXEL_WIDTH - 2, MARGIN_HEIGHT * LEN * PIXEL_HEIGHT); // "HOLD"가 4글자기 때문에 -2로 중앙정렬
-        cout << "HOLD";
+
+        lazyPrinter.setColor(ConsoleColor::BORDER_DEFAULT);
+        lazyPrinter.setXY(MARGIN_WIDTH / 2 * LEN * PIXEL_WIDTH - 2, MARGIN_HEIGHT * LEN * PIXEL_HEIGHT); // "HOLD"가 4글자기 때문에 -2로 중앙정렬
+        lazyPrinter.text("HOLD");
 
         // holded Block
         int hx = -MARGIN_WIDTH / 2 - holdedBlock.getCenterX();
         int hy = MARGIN_HEIGHT / 2 + 2; // - holdedBlock.getCneterY();
 
-        setColor(tetrominoColor[holdedBlock.getKind()]);
-        for (auto [x, y] : holdedBlock.getShape()) {
-            drawGrid(x + hx, y + hy);
-        }
+        lazyPrinter.setColor(tetrominoColor[holdedBlock.getKind()]);
+        for (auto [x, y] : holdedBlock.getShape()) drawGrid(x + hx, y + hy);
     }
 
     bool inRange(int x, int y) {
@@ -202,11 +211,6 @@ private:
         timer_drop.init();
 
         // TODO nextBlock 추가
-
-        // TODO test용 코드 삭제해야됨
-        gotoxy(40, 0);
-        cout << "KIND : " << kind << " ::: ";
-        for (auto [x, y] : fallingBlock.getCoordinates()) cout << "(" << x << ", " << y << "), ";
     }
 
     void update() {
@@ -225,17 +229,22 @@ private:
     }
 
     void display() {
-        // TODO 출력 최적화 해야 될 듯. 업데이트된 좌표만 출력하게 해야 될 듯 화면 깜빡임 너무 심함
         deleteMargin();
         drawBorder();
         drawBoard();
         drawFallingBlock();
-
         drawHold();
+
+        lazyPrinter.render();
     }
 
 public:
-    Game() : board(ROWS, vector<int>(COLS)), timer_drop(17, true), timer_hardDropped(1, false), timer_breakRow(4, true) {
+    Game(int sleepTime) : \
+        board(ROWS, vector<int>(COLS)), lazyPrinter((MARGIN_WIDTH + COLS + 2) * LEN, (MARGIN_HEIGHT + ROWS + 2) * LEN), \
+        timer_drop(dropTime / sleepTime, true), \
+        timer_hardDropped(hardDropAnimationTime / sleepTime, false), \
+        timer_breakRow(breakRowAnimationTime / sleepTime, true) {
+
         // border
         borderPos.reserve(ROWS + COLS + 2 << 1);
 
---
+++ lazyPrinter.h
@@ -0,0 +1,91 @@
+#ifndef LAZYPRINTER_H
+#define LAZYPRINTER_H
+
+#include <iostream>
+#include <vector>
+#include <windows.h>
+#include "ConsoleColor.h"
+using namespace std;
+
+const char PIXEL_CHAR = 'X';
+const int PIXEL_WIDTH = 2;
+const int PIXEL_HEIGHT = 1;
+
+class LazyPrinter {
+private:
+    const int WIDTH;
+    const int HEIGHT;
+
+    struct Data {
+        char ch;
+        int color;
+
+        bool operator!=(const Data &other) const { return ch != other.ch || color != other.color; }
+    };
+
+    int curColor;
+    int curX, curY;
+    vector<vector<Data> > prevConsole, console;
+
+    void lazyPrint(char ch) {
+        if (curX >= 0 && curX < WIDTH && curY >= 0 && curY < HEIGHT) console[curX++][curY] = Data{ch, curColor};
+        else cerr << "out of bounds at (" << curX << ", " << curY << ")\n";
+    }
+
+    int convertColorToInt(ConsoleColor color, ConsoleColor bgColor = ConsoleColor::ORIGINALBG) {
+        int colorInt = static_cast<int>(color) & 0xf;
+        int bgColorInt = static_cast<int>(bgColor) & 0xf;
+        return (bgColorInt << 4) | colorInt;
+    }
+
+    void printOnConsole(short x, short y, int colorData, char ch) {
+        SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), {x, y});
+        SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), colorData);
+        cout << ch;
+    }
+
+public:
+    LazyPrinter(int width, int height) : \
+        WIDTH(width * PIXEL_WIDTH), HEIGHT(height * PIXEL_HEIGHT), \
+        curColor(convertColorToInt(ConsoleColor::WHITE)), curX(0), curY(0), \
+        prevConsole(WIDTH, vector<Data>(HEIGHT, {PIXEL_CHAR, curColor})), \
+        console(WIDTH, vector<Data>(HEIGHT, {PIXEL_CHAR, curColor})) {}
+
+    void setColor(ConsoleColor color, ConsoleColor bgColor = ConsoleColor::ORIGINALBG) {
+        curColor = convertColorToInt(color, bgColor);
+    }
+
+    void setXY(int gx, int gy) {
+        curX = gx;
+        curY = gy;
+    }
+
+    void rect(int x, int y, int w, int h) { // 좌상단 꼭짓점 좌표 (x, y)부터 가로 w, 세로 h개의 픽셀 표시
+        w *= PIXEL_WIDTH;
+        h *= PIXEL_HEIGHT;
+
+        for (int i = 0; i < h; i++) {
+            setXY(x * PIXEL_WIDTH, y * PIXEL_HEIGHT + i);
+            for (int _ = w; _--;) lazyPrint(PIXEL_CHAR);
+        }
+    }
+
+    void text(string st) {
+        for (auto ch : st) lazyPrint(ch);
+    }
+
+    void render() {
+        for (int x = 0; x < WIDTH; x++) {
+            for (int y = 0; y < HEIGHT; y++) {
+                if (prevConsole[x][y] != console[x][y]) {
+                    printOnConsole(x, y, console[x][y].color, console[x][y].ch);
+                    prevConsole[x][y] = console[x][y];
+                }
+            }
+        }
+    }
+
+
+};
+
+#endif
